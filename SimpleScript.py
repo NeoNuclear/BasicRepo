9a2b54cb1b3a9a3da42d7c8986a8a3377577c89e
Zain <zainelgammel@gmail.com>
2025-03-12 10:21:39 +0800

    Updated python script

--- SimpleScript.py
+++ SimpleScript.py
@@ -1,5 +1,7 @@
-counter = 0
+import random
+money = 0
 userInput = int(input("Enter loop amount: "))
 for i in range(userInput):
-    counter += 1
-    print(counter)
+    money += 1
+    money += 12
+money += random.randint(1,100)
